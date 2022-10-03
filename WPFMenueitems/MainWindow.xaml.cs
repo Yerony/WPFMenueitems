c5370d6bef282080f8d89ceeb5aae64c5dea2930
Yerony <okakipur@web.de>
2022-10-03 14:50:57 +0200

    added MenuStruc.xaml to repo

--- WPFMenueitems/MainWindow.xaml.cs
+++ WPFMenueitems/MainWindow.xaml.cs
@@ -31,23 +31,16 @@ namespace WPFMenueitems
             {
                 string fileText = File.ReadAllText(menufilepath);
                 Object obj = XamlServices.Parse(fileText);
-                Menu adf = new Menu();
-                adf.Loaded += Lo12;
+                //Menu adf = new Menu();
+                //adf.Loaded += Lo12;
 
                 dockPanel1.Children.Add(obj as UIElement);
             }
             else { MessageBox.Show("ERROR: File not found"); }
-            for (int i = 0; i < dockPanel1.Children.Count; i++)
-            { 
-                //internal System.Windows.Controls.MenuItem benutzer1;
-                //var mitem = Benutzer1;
-                if (dockPanel1.Children[i].GetType().ToString() == "Benutzer1") MessageBox.Show("drrrrr");
-                //MessageBox.Show(mitem.Tag.ToString());
-            }
         }
         public void Lo12(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("mitem.Tag.ToString()");
+            MessageBox.Show("Lo12event");
         }
     }
 }
